dd86a0d21eec2dee5c80d03fb3dd03d06309d5ac
Samuel Kubai Kamau <samuel.kubai@andela.com>
2019-06-10 19:24:39 +0300

    story(process): fix the process.h file

--- Process.h
+++ Process.h
@@ -48,11 +48,11 @@ string Process::getCmd() const {
 }
 
 int Process::getCpu() const {
-    return this->cpu;
+    return stoi(this->cpu);
 }
 
 int Process::getMem() const {
-    return this->mem;
+    return stoi(this->mem);
 }
 
 string Process::getUpTime() const {
