5c25d912e6b89ad58226f4fd9ecabc05f9a60715
Samuel Kubai Kamau <samuel.kubai@andela.com>
2019-06-10 19:21:24 +0300

    story(process): implement the process class print function

--- Process.h
+++ Process.h
@@ -19,11 +19,7 @@ public:
     Process(string pid){
         this->pid = pid;
         this->user = ProcessParser::getProcUser(pid);
-        //TODOs:
-        //complete for mem
-        //complete for cmd
-        //complete for upTime
-        //complete for cpu
+        this->cmd  = ProcessParser::getCmd(pid);
     }
     void setPid(int pid);
     string getPid()const;
@@ -34,18 +30,41 @@ public:
     string getUpTime()const;
     string getProcess();
 };
+
 void Process::setPid(int pid){
     this->pid = pid;
 }
+
 string Process::getPid()const {
     return this->pid;
 }
-string Process::getProcess(){
+
+string getUser()const {
+    return this->user;
+}
+
+string getCmd() const {
+    return this->cmd;
+}
+
+int getCpu() const {
+    return this->cpu;
+}
+
+int getMem() const {
+    return this->mem;
+}
+
+string getUpTime() const {
+    return this->upTime;
+}
+
+string Process::getProcess() {
     if(!ProcessParser::isPidExisting(this->pid))
         return "";
     this->mem = ProcessParser::getVmSize(this->pid);
     this->upTime = ProcessParser::getProcUpTime(this->pid);
     this->cpu = ProcessParser::getCpuPercent(this->pid);
 
-    return (this->pid + "   "); //TODO: finish the string! this->user + "   "+ mem...cpu...upTime...;
+    return (this->pid + "   " + this->user + "   " + this->cpu +  "   " + this->mem +  "   " + this->upTime +  "   " + this->cmd); 
 }
